a4d9c8029b80a5643fbfcdcdfa6b5a154e76aaa1
jieun318 <jieun031800@naver.com>
2026-01-16 16:13:53 +0900

    fix: remove illegal whitespace in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      {' '}
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
